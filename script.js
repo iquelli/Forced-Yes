0f8ad07ab5b36eeca9d4bce70344c4cd5e1d2d23
Kelli <93059659+iquelli@users.noreply.github.com>
2023-05-16 23:40:31 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const content = document.querySelector(".content");
+const yes_bttn = document.querySelector(".yes-button");
+const no_bttn = document.querySelector(".no-button");
+const question = document.querySelector(".question");
+
+const content_rect = content.getBoundingClientRect();
+const bttn_rect = no_bttn.getBoundingClientRect();
+
+yes_bttn.addEventListener('click', () => {
+    question.innerHTML = 'Cannot wait to see you ;)'
+});
+
+no_bttn.addEventListener('mouseover', () => {
+    const posX = Math.floor(Math.random() * (content_rect.width - bttn_rect.width));
+    const posY = Math.floor(Math.random() * (content_rect.height - bttn_rect.height));
+
+    no_bttn.style.left = posX + 'px';
+    no_bttn.style.top = posY + 'px';
+});
